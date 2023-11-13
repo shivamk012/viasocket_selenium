1f5d7e30728c49c5977ccc1d56c3f004001cc4e0
shivamk012 <shivamkoolwal14@gmail.com>
2023-11-13 11:42:46 +0530

    completed variable and api step test cases

--- test/pages/Page/page.js
+++ test/pages/Page/page.js
@@ -2,6 +2,7 @@ const {Builder, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const fs = require('fs');
 const resemble = require('resemblejs');
+const Jimp = require('jimp');
 module.exports = class Page {
     constructor(){
         try{
@@ -12,15 +13,29 @@ module.exports = class Page {
             this.driver = new Builder().setChromeOptions(options).forBrowser('chrome').build();
             console.log('Driver created successfully');
             this.app_link = (process.argv[2] === "test" ? process.env.TEST_LINK : process.env.PROD_LINK);
-
+            this.mode = process.argv[4] === "capture";
         }
         catch(err){
             console.log(err);
         }
     }
 
+    async compareImages(imagePath1, imagePath2) {
+        return new Promise((resolve, reject) => {
+          resemble(imagePath1)
+            .compareTo(imagePath2)
+            .onComplete(data => resolve(data))
+            .ignoreLess()
+            .onComplete(data => resolve(data));
+        });
+      }
+
     get Driver(){
         return this.driver;
+    }
+
+    get isCaptureMode(){
+        return this.mode
     }
 
     currentUrl(endpoint){
@@ -84,16 +99,44 @@ module.exports = class Page {
         await this.driver.executeScript("arguments[0].style.filter = 'blur(10px)';" , webelement);
     }
 
-    async takeScreenShotAndSave(imagePath){
-        const FunctionRefrenceScreenshot = await this.driver.takeScreenshot();
-        fs.writeFileSync(imagePath , FunctionRefrenceScreenshot , 'base64');
-    }
+    async takeScreenShotAndCrop(element , imagePath){
+        // const screenShot = await this.driver.takeScreenshot();
+        // const rect = await element.getRect();
 
-    async compareScreenShot(referenceImagePath , specImagePath , comparisonImagePath){
+        // // Calculate the region to crop
+        // const left = rect.x;
+        // const top = rect.y;
+        // const width = rect.width;
+        // const height = rect.height;
+        // sharp(screenShot)
+        // .extract({ left: left, top: top, width: width, height: height })
+        // .toFile('./functionflow.png', function (err) {
+        //     if (err) console.log(err);
+        // })
+        
+        const location = await element.getRect();
+
+        // Take a screenshot of the entire page
+        const screenshot = await this.driver.takeScreenshot();
+
+        // Use Jimp to process the image
+        const image = await Jimp.read(Buffer.from(screenshot, 'base64'));
+        
+        // Crop the image to the region of the element
+        console.log(location);
+        image.crop(location.x, location.y, location.width, location.height);
+        
+        // Save the cropped image
+        if(process.argv[4] === "capture") await image.writeAsync(`./test/ReferenceImages/${imagePath}`);
+        else await image.writeAsync(`./test/SpecImages/${imagePath}`);
+    }
+    
+    async compareScreenShot(imagePath){
         return new Promise(async(resolve , reject) => {
             try{
-                const comparisonResult = await compareImages(referenceImagePath, specImagePath);
-                fs.writeFileSync(comparisonImagePath, comparisonResult.getBuffer());
+                if(process.argv[4] === "capture") resolve("capture");
+                const comparisonResult = await this.compareImages(`./test/ReferenceImages/${imagePath}`, `./test/SpecImages/${imagePath}`);
+                fs.writeFileSync(`./test/ComparisonImages/${imagePath}`, comparisonResult.getBuffer());
                 resolve(comparisonResult);
             }catch(err){
                 reject(err);
--- test/specs/testApiStep.spec.js
+++ test/specs/testApiStep.spec.js
@@ -1,39 +1,93 @@
 const FlowPage = require('../pages/Flow/flow');
 const {endpoints , stepIndex , apiIndex} = require('../enums');
+const {expect} = require('chai');
 
-const flowPage = new FlowPage();
+let flowPage;
 
 async function testApiStep(methodType){
-    return new Promise(async(resolve , reject) => {
+    describe('test api step slider' , () => {
+        before(() => {
+            flowPage = new FlowPage();
+        })
+
+        it('should render function step' , async() => {
         try{
-            await flowPage.open(endpoints.HOME);
-            await flowPage.clickOnLoginWithGoogle();
-            await flowPage.waitForEndpoint(endpoints.PROJECT , 60000);
-            await flowPage.clickOnProjectName();
-            await flowPage.waitForScriptSlider();
-            await flowPage.clickOnScript();
-            await flowPage.waitForFlowPageToOpen();
-            await flowPage.clickOnEditButton();
-            await flowPage.clickOnAddSteps();
-            await flowPage.getAllSteps();
-            await flowPage.clickOnStep(stepIndex.API);
-            await flowPage.blurLogSlider();
-            await flowPage.blurWorkFlow();
-            await flowPage.takeScreenShotAndSave('./functionslider.png')
+                await flowPage.open(endpoints.HOME);
+                await flowPage.clickOnLoginWithGoogle();
+                await flowPage.waitForEndpoint(endpoints.PROJECT , 60000);
+                await flowPage.clickOnProjectName();
+                await flowPage.waitForScriptSlider();
+                await flowPage.clickOnScript();
+                await flowPage.waitForFlowPageToOpen();
+                await flowPage.clickOnEditButton();
+                await flowPage.clickOnAddSteps();
+                await flowPage.getAllSteps();
+                await flowPage.clickOnStep(stepIndex.API);
+                await flowPage.takeScreenShotFunctionSlider('apiSlider');
+                const comparisonResult = await flowPage.compareScreenShot('apiSlider'); 
+                const isCaptureMode = await flowPage.isCaptureMode;
+                if(isCaptureMode) return;
+                const num = Math.floor(comparisonResult.rawMisMatchPercentage);
+                expect(num).to.be.lessThan(20);
+            }catch(err){
+                console.log(err);
+            }
+        })
+
+        it('api step name should be editable' , async() => {
             await flowPage.initialiseApiSlider();
             await flowPage.fillStepName('api_step');
-            await flowPage.selectApiMethod(apiIndex[methodType]);
+            const stepname = await flowPage.getStepName();
+            expect(stepname).to.be.equal('api_step');
+        })
+
+        it('api url field should be editable' , async() => {
             await flowPage.fillUrl(process.env.GET_REQUEST_URL);
+            const url = await flowPage.getUrl();
+            expect(url).to.be.equal(process.env.GET_REQUEST_URL);
+        })
+
+        it('api method should be editable' , async() => {
+            await flowPage.selectApiMethod(apiIndex[methodType]);
+            const method = await flowPage.getSelectedApiMethod();
+            expect(method).to.be.equal(methodType);
+        })
+
+        it('dry run should produce a response' , async()=>{
             if(apiIndex[methodType] === 1 || api[methodType] === 2 || api[methodType] === 4){
                 await flowPage.sendBodyData(apiIndex[methodType]);
             } 
             await flowPage.clickOnDryRunButton();
+            const response = await flowPage.getResponseData();
+            expect(response).to.not.be.null;
+        })
+
+        it('should have correct resposne' , async() => {
+            await flowPage.takeResponseScreenShot('apiResponse.png');
+            const result = await flowPage.compareScreenShot('apiResponse.png');
+            const isCaptureMode = await flowPage.isCaptureMode;
+            if(isCaptureMode) return;
+            const num = Math.floor(result.rawMisMatchPercentage);
+            expect(num).to.be.lessThan(20);
+        })
+
+        it('api slider input fields should not become empty on dry run click' , async() => {
+            await flowPage.takeScreenShotFunctionSlider('filledApiSlider.png');
+            const comparisonResult = await flowPage.compareScreenShot('filledApiSlider.png');
+            const isCaptureMode = await flowPage.isCaptureMode;
+            if(isCaptureMode) return;
+            const num = Math.floor(comparisonResult.rawMisMatchPercentage);
+            expect(num).to.be.lessThan(20);
+        })
+
+        it('should create a api step in flow' , async() => {
             await flowPage.clickOnCreateButton();
-        }catch(err){
-                reject(err);
-        }finally{
-            // await flowPage.close();
-        }
+
+        })
+
+        // after(async()=>{
+        //     await flowPage.close();
+        // })
     })
 }
 
